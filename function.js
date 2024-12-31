b78829b8b1d73ef61088b90c71417d6ac7f3873b
lSocial-Projectl <sproject1010@gmail.com>
2024-12-31 22:33:45 +0530

    Update function.js

--- function.js
+++ function.js
@@ -9,7 +9,7 @@
     
     
 function Share(){
-  const message = "🎊.HAPPY NEW YEAR.🎊 😁 Best wishes from your friend. 🥳This Year will bring you so much Joy , Fun , and lots of Happy.... Check out this line 👇👇👇"+"https://google.com"; 
+  const message = "🎊.HAPPY NEW YEAR.🎊 😁 Best wishes from your friend. 🥳This Year will bring you so much Joy , Fun , and lots of Happy.... Check out this line 👉"+"https://lsocial-projectl.github.io/Wish/"; 
   const whatsappURL = `https://wa.me/?text=${encodeURIComponent(message)}`;
   
   // Redirect to WhatsApp with the pre-filled message
